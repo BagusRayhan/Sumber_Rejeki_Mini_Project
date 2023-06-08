e254757857cfb8c10817c5ac764dd94dba437797
ddckyy01 <130640162+ddckyy01@users.noreply.github.com>
2023-06-08 15:59:56 +0700

    tesss

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,32 +2,7 @@
 <html lang="en">
 
 <!-- Mirrored from colorlib.com/etc/lf/Login_v4/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 08 Jun 2023 06:45:50 GMT -->
-<head>
-<title>Login V4</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/vendor/bootstrap/css/bootstrap.min.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/fonts/iconic/css/material-design-iconic-font.min.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/vendor/animate/animate.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/vendor/css-hamburgers/hamburgers.min.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/vendor/animsition/css/animsition.min.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/vendor/select2/select2.min.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/vendor/daterangepicker/daterangepicker.css') }}">
-
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/css/util.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('colorlib/css/main.css') }}">
-
-<meta name="robots" content="noindex, follow">
-<script nonce="0b346717-10af-42c9-8801-f9ff4d347d27">(function(w,d){!function(dK,dL,dM,dN){dK[dM]=dK[dM]||{};dK[dM].executed=[];dK.zaraz={deferred:[],listeners:[]};dK.zaraz.q=[];dK.zaraz._f=function(dO){return function(){var dP=Array.prototype.slice.call(arguments);dK.zaraz.q.push({m:dO,a:dP})}};for(const dQ of["track","set","debug"])dK.zaraz[dQ]=dK.zaraz._f(dQ);dK.zaraz.init=()=>{var dR=dL.getElementsByTagName(dN)[0],dS=dL.createElement(dN),dT=dL.getElementsByTagName("title")[0];dT&&(dK[dM].t=dL.getElementsByTagName("title")[0].text);dK[dM].x=Math.random();dK[dM].w=dK.screen.width;dK[dM].h=dK.screen.height;dK[dM].j=dK.innerHeight;dK[dM].e=dK.innerWidth;dK[dM].l=dK.location.href;dK[dM].r=dL.referrer;dK[dM].k=dK.screen.colorDepth;dK[dM].n=dL.characterSet;dK[dM].o=(new Date).getTimezoneOffset();if(dK.dataLayer)for(const dX of Object.entries(Object.entries(dataLayer).reduce(((dY,dZ)=>({...dY[1],...dZ[1]})),{})))zaraz.set(dX[0],dX[1],{scope:"page"});dK[dM].q=[];for(;dK.zaraz.q.length;){const d_=dK.zaraz.q.shift();dK[dM].q.push(d_)}dS.defer=!0;for(const ea of[localStorage,sessionStorage])Object.keys(ea||{}).filter((ec=>ec.startsWith("_zaraz_"))).forEach((eb=>{try{dK[dM]["z_"+eb.slice(7)]=JSON.parse(ea.getItem(eb))}catch{dK[dM]["z_"+eb.slice(7)]=ea.getItem(eb)}}));dS.referrerPolicy="origin";dS.src="../../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(dK[dM])));dR.parentNode.insertBefore(dS,dR)};["complete","interactive"].includes(dL.readyState)?zaraz.init():dK.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
+@include('template.head')
 <body>
 <div class="limiter">
 <div class="container-login100" style="background-image: url('{{ asset('colorlib/images/bg-01.jpg') }}');">
@@ -36,10 +11,10 @@
 <span class="login100-form-title p-b-49">
 Login
 </span>
-<div class="wrap-input100 validate-input m-b-23" data-validate="Username is reauired">
-<span class="label-input100">Username</span>
-<input class="input100" type="text" name="username" placeholder="Type your username">
-<span class="focus-input100" data-symbol="&#xf206;"></span>
+<div class="wrap-input100 validate-input m-b-23" data-validate="Email is reauired">
+<span class="label-input100">Email</span>
+<input class="input100" type="text" name="email" placeholder="Type your email">
+<span class="focus-input100" data-symbol="&#xf199;"></span>
 </div>
 <div class="wrap-input100 validate-input" data-validate="Password is required">
 <span class="label-input100">Password</span>
