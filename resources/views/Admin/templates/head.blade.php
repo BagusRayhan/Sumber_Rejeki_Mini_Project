442ba429942886fd04d425ece5d9a98c7f6f441d
Harja <rachmadsuharja123@gmail.com>
2023-06-08 16:01:27 +0700

    edit sidebar dan head

--- resources/views/Admin/templates/head.blade.php
+++ resources/views/Admin/templates/head.blade.php
@@ -11,8 +11,8 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com/">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&family=Heebo:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+
     <!-- Icon Font Stylesheet -->
     <link href="{{ asset('ProjectManagement/cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css') }}" rel="stylesheet">
     <link href="{{ asset('ProjectManagement/cdn.jsdelivr.net/npm/bootstrap-icons%401.4.1/font/bootstrap-icons.css') }}" rel="stylesheet">
