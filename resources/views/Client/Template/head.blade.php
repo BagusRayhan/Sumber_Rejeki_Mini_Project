6e0e9bbd86bd6f849ca73e25839f053474466a1b
Harja <rachmadsuharja123@gmail.com>
2023-06-12 06:41:21 +0700

    Memperbaiki Halaman Client
    1. Memperbaiki sidebar di halaman client
    2. Menambahkan link cdn font awesome & bootstrap versi terbaru di file head.blade.php untuk client

--- resources/views/Client/Template/head.blade.php
+++ resources/views/Client/Template/head.blade.php
@@ -8,14 +8,12 @@
     <link href="{{ asset('ProjectManagement/dashmin/img/favicon.html') }}" rel="icon">
 
     <!-- Google Web Fonts -->
-      <script src="https://kit.fontawesome.com/42d2fda859.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com/">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Ubuntu:wght@400;500;700&family=Heebo:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
     
     <!-- Icon Font Stylesheet -->
-    <link href="{{ asset('ProjectManagement/cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('ProjectManagement/cdn.jsdelivr.net/npm/bootstrap-icons%401.4.1/font/bootstrap-icons.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Libraries Stylesheet -->
     <link href="{{ asset('ProjectManagement/dashmin/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
@@ -23,6 +21,10 @@
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{ asset('ProjectManagement/dashmin/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 
     <!-- Template Stylesheet -->
     <link href="{{ asset('ProjectManagement/dashmin/css/style.css') }}" rel="stylesheet">
--- resources/views/Client/Template/sidebar.blade.php
+++ resources/views/Client/Template/sidebar.blade.php
@@ -3,18 +3,21 @@
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
-                <a href="index.html" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary" style="font-size:150%; margin-top:2%;"><img src="{{ asset('ProjectManagement/dashmin/img/logo.png') }}" style="width: 35%; height:35%; margin-bottom:9%;">PROREQ</h3>
+                <a href="#" class="navbar-brand mx-4 mb-3 justify-content-center">
+                    <h3 class="text-dark" style="font-family: 'Fugaz One">
+                        <img src="{{ asset('ProjectManagement/dashmin/img/logo.png') }}" class="w-25">
+                        PROREQ
+                    </h3>
                 </a>
                 <div class="navbar-nav w-100" >
                     <a href="{{ route('indexclient') }}" class="nav-item nav-link {{ Request::routeIs('indexclient*') ? 'active' : '' }}"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
                     <div class="nav-item dropdown">
                         <a href="{{ route('drequestclient') }}" class="nav-link dropdown-toggle {{ Request::routeIs('drequestclient*','setujuclient*','selesaiclient*','ditolakclient*') ? 'active' : '' }}" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Project</a>
-                        <div class="dropdown-menu bg-transparent border-0" style="margin-left: 20%;">
-                            <a href="{{ route('drequestclient') }}" class="dropdown-item {{ Request::routeIs('drequestclient*') ? 'active' : '' }}">Project Request</a>
-                            <a href="{{ route('setujuclient') }}" class="dropdown-item {{ Request::routeIs('setujuclient*') ? 'active' : '' }}">Project Disetujui</a>
-                            <a href="{{ route('selesaiclient') }}" class="dropdown-item {{ Request::routeIs('selesaiclient*') ? 'active' : '' }}">Project Selesai</a>
-                            <a href="{{ route('ditolakclient') }}" class="dropdown-item {{ Request::routeIs('ditolakclient*') ? 'active' : '' }}">Project Ditolak</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="{{ route('drequestclient') }}" class="dropdown-item py-2 px-5 fw-medium {{ Request::routeIs('drequestclient*') ? 'text-primary' : '' }}">Project Request</a>
+                            <a href="typography.html" class="dropdown-item py-2 px-5 fw-medium {{ Request::routeIs('setujuclient*') ? 'text-primary' : '' }}">Project Disetujui</a>
+                            <a href="element.html" class="dropdown-item py-2 px-5 fw-medium {{ Request::routeIs('selesaiclient*') ? 'text-primary' : '' }}">Project Selesai</a>
+                            <a href="{{ route('ditolakclient') }}" class="dropdown-item py-2 px-5 fw-medium {{ Request::routeIs('ditolakclient*') ? 'text-primary' : '' }}">Project Ditolak</a>
                         </div>
                     </div>
                     <a href="{{ route('bayarclient') }}" class="nav-item nav-link {{ Request::routeIs('bayarclient*') ? 'active' : '' }}"><i class="fa-solid fa-wallet"></i>Transaksi</a>
