2f96165f21c2193d1dd464aba0b3f939776c692d
ddckyy01 <130640162+ddckyy01@users.noreply.github.com>
2023-06-12 08:03:52 +0700

    createproreq

--- resources/views/Client/createproreq.blade.php
+++ resources/views/Client/createproreq.blade.php
@@ -24,11 +24,9 @@
         .td-spacing {
             margin-right: 20px;
         }
-        td {
-        margin-top: 20px;
-    }
+
     </style>
-     <table>
+     <table style="margin-top: 20px">
         <tr>
             <td style="padding-right: 25px;"></td>
             <td width="50%">
@@ -52,35 +50,113 @@
                                 <label for="input4">Deadline</label>
                                 <input type="datetime-local" class="form-control" id="input4" placeholder="Input 4">
                             </div><br>
-                            <button type="submit" class="btn btn-primary">Kirim Request</button>
+                            <center>
+                            <button type="submit" class="btn btn-primary">Kirim Request</button><br><br>
+                            <a  href="http://127.0.0.1:8000/drequestclient">Kembali</a></center>
+                        </form>
+                    </div>
+                </div>
+            </td>
+            <style>
+.input-group {
+  position: relative;
+}
+
+.input-icon {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  pointer-events: auto;
+  cursor: pointer;
+}
+
+.dropdown-menu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  right: 0;
+  z-index: 999;
+  /* Gaya lainnya yang diinginkan */
+}
+
+.dropdown-options {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+  position: absolute;
+  top: 100%;
+  right: 0;
+  width: 200px;
+}
+
+.dropdown-options li {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.dropdown-options li:hover {
+  background-color: #f5f5f5;
+}
+
+.option-icon {
+  margin-right: 5px;
+}
+
+
+            </style>
+            <td style="padding-right: 25px;"></td>
+            <td width="50%">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Fitur</h5>
+                        <form>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="input1" placeholder="Add fitur">
+                                <div class="input-icon">
+                                          <i class="fas fa-ellipsis-v"></i>
+
+                                </div>
+                                <div class="dropdown-menu">
+                                    <ul class="dropdown-options">
+                                        <li><span class="option-icon"><i class="fas fa-plus"></i></span> Tambah deskripsi</li>
+                                    </ul>
+                                </div>
+                            </div><br>
+                            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+  $('.input-icon').click(function() {
+    $(this).siblings('.dropdown-menu').toggle();
+  });
+});
+
+</script>
+                            <div class="form-group">
+                                <label for="input2">Fitur 2</label>
+                                <input type="text" class="form-control" id="input2" placeholder="Add fitur">
+                            </div><br>
+                            <div class="form-group">
+                                <label for="input3">Fitur 3</label>
+                                <input type="text" class="form-control" id="input3" placeholder="Add fitur">
+                            </div><br>
+                            <div class="form-group">
+                                <label for="input4">Fitur 4</label>
+                                <input type="text" class="form-control" id="input4" placeholder="Add fitur">
+                            </div><br>
+                            <center>
+                            <button type="submit" class="btn btn-primary">Kirim Request</button><br><br>
+                            <a  href="http://127.0.0.1:8000/drequestclient">Kembali</a></center>
                         </form>
                     </div>
                 </div>
             </td>
 
-            <td style="padding-right: 25px; margin-top: 20px;"></td>
-            <td width="50%">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Fitur Fitur</h5>
-                        <form>
-                            <div class="form-group">
-                                <label for="input1">Fitur</label>
-                                <input type="text" class="form-control" id="input1" placeholder="masukkan fitur yang anda inginkan">
-                            </div><br><br>
-                            <div class="form-group">
-                                <input type="text" class="form-control" id="input2" placeholder="Masukkan fitur">
-                            </div><br><br>
-                            <div class="form-group">
-                                <input type="text" class="form-control" id="input3" placeholder="masukkan fitur">
-                            </div><br><br>
-                            <div class="form-group">
-                                <input type="te" class="form-control" id="input4" placeholder="masukkan fitur">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </td>
+
         </tr>
      </table>
 
