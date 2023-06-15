a740cd1f9ddbc66c756007e81fd33461807011cc
BagusRayhan <110075643+BagusRayhan@users.noreply.github.com>
2023-06-15 11:03:59 +0700

    ganti foto profil

--- resources/views/Client/Template/navbar.blade.php
+++ resources/views/Client/Template/navbar.blade.php
@@ -1,4 +1,28 @@
             <!-- Navbar Start -->
+
+            <style>
+                .profile {
+                position: relative;
+                display: inline-block;
+              }
+              
+              .profile-image {
+                width: 200px;
+                height: 200px;
+                object-fit: cover;
+              }
+              
+              .change-profile-button {
+                position: absolute;
+                bottom: 10px;
+                right: 10px;
+                background-color: #fff;
+                border-radius: 50%;
+                padding: 8px;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+              }
+            </style>
+              
             <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
                 <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
@@ -57,7 +81,7 @@
                                 <div class="row mb-3 mt-sm-2">
                                   <button type="button" style="margin-right:2%;" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 <div class="form-group">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel"><center><img src="{{ asset('ProjectManagement/dashmin/img/user2.png') }}" alt="" style="width:40%; height:40%;"><br>Suharjo</center></h1>
+                                <h1 class="modal-title fs-5" id="exampleModalLabel"><center><img src="{{ asset('ProjectManagement/dashmin/img/user-new.png') }}" alt="" style="width:40%; height:40%;"><br>Suharjo</center></h1>
                                 </div>
                                 <div class="form-group w-100 p-3">
                                     <label for="input1">Nama</label>
@@ -100,14 +124,39 @@
                                <div class="form-group">
                                 <h1 class="modal-title fs-5" id="exampleModalLabel">
                                     <center>
-                                    <label for="file-upload">
-                                        <img id="profile-image" src="{{ asset('ProjectManagement/dashmin/img/user.png') }}" alt="" style="width:40%; height:40%; border-radius: 50%;">
-                                        <br>Suharjo
-                                    </label>
+                                        <div class="profile">
+                                            <img src="{{ asset('ProjectManagement/dashmin/img/user-new.png') }}" alt="Gambar Profil" class="profile-image">
+                                            <a href="#" type="file" class="change-profile-button" id="chooseFileButton" style="width: 45px">
+                                              <i class="fas fa-camera"></i>
+                                              <input type="file" id="fileInput" style="display:none" accept=".jpg,.png,.pdf">
+                                            </a>
+                                          </div>                                          
                                     <input id="file-upload" type="file" style="display: none;">
                                     </center>
                                 </h1>
                                 </div>
+                                <script>
+                                    document.getElementById('chooseFileButton').addEventListener('click', function() {
+                                    document.getElementById('fileInput').click();
+                                    });
+
+                                    document.getElementById('fileInput').addEventListener('change', function() {
+                                    var selectedFile = this.files[0];
+                                    // Lakukan sesuatu dengan file yang dipilih, misalnya mengunggahnya ke server
+                                    console.log('Selected file:', selectedFile);
+                                    });
+
+                                </script>
+                                <script>
+                                $(document).ready(function() {
+                                $('.change-profile-button').on('click', function(e) {
+                                    e.preventDefault();
+                                    // Tambahkan kode yang ingin Anda jalankan ketika tombol perubahan profil diklik
+                                    // Misalnya, tampilkan dialog atau tampilkan form perubahan profil
+                                });
+                                });
+
+                                </script>
                                 <script>
                                     document.getElementById("file-upload").addEventListener("change", function(event) {
                                         var input = event.target;
