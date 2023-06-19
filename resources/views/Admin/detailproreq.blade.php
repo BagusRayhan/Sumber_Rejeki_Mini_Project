1c9651d468d5831692e3a7ef7322050fd76b63cb
Harja <rachmadsuharja123@gmail.com>
2023-06-19 09:48:08 +0700

    Memperbaiki Navbar di Halaman Client
    Menghapus navbar lama & mengubahnya dengan navbar yang sama di halaman admin

--- resources/views/Admin/detailproreq.blade.php
+++ resources/views/Admin/detailproreq.blade.php
@@ -154,23 +154,6 @@
 
     </div>
 
-    <!-- JavaScript Libraries -->
-    <script src="{{ asset('ProjectManagement/code.jquery.com/jquery-3.4.1.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/cdn.jsdelivr.net/npm/bootstrap%405.0.0/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/chart/chart.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/easing/easing.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/waypoints/waypoints.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/tempusdominus/js/moment.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
-    <script src="{{ asset('ProjectManagement/dashmin/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-
-    <!-- Template Javascript -->
-    <script src="{{ asset('ProjectManagement/dashmin/js/main.js') }}"></script>
-    <script>
-        var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-    </script>
     
+    @include('Admin.templates.script')
 </body>
-
-@include('Admin.templates.script')
