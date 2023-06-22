4493c1cd9b1f52ab63ba66f43f88ba1ddfe6fbd8
BagusRayhan <110075643+BagusRayhan@users.noreply.github.com>
2023-06-22 09:16:28 +0700

    route web middleware

--- routes/web.php
+++ routes/web.php
@@ -32,26 +32,29 @@ Route::get('register', [AuthController::class, 'register'])->name('register');
 Route::post('postsignup', [AuthController::class, 'signupsave'])->name('postsignup');
 Route::get('forgot', [AuthController::class, 'forgot'])->name('forgot');
 Route::get('kebijakan', [AuthController::class, 'kebijakan'])->name('kebijakan');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-// Halaman Client
-Route::get('indexclient', [IndexcController::class, 'indexclient'])->name('indexclient');
-Route::get('drequestclient', [IndexcController::class, 'drequestclient'])->name('drequestclient');
-Route::get('createproreq', [IndexcController::class, 'createproreq'])->name('createproreq');
-Route::post('simpanpro', [IndexcController::class, 'simpan'])->name('simpanpro');
-Route::get('requestclient', [IndexcController::class, 'requestclient'])->name('requestclient');
-Route::get('editproreq/{id}', [IndexcController::class, 'editproreq'])->name('editproreq');
-Route::put('updateproreq/{id}', [IndexcController::class, 'update'])->name('updateproreq');
-Route::get('setujuclient', [SetujuController::class, 'setujuclient'])->name('setujuclient');
-Route::get('selesaiclient', [SelesaiController::class, 'selesaiclient'])->name('selesaiclient');
-Route::get('revisiclient', [SelesaiController::class, 'revisiclient'])->name('revisiclient');
-Route::get('ditolakclient', [TolakController::class, 'ditolakclient'])->name('ditolakclient');
-Route::get('bayarclient', [BayarController::class, 'bayarclient'])->name('bayarclient');
-Route::get('bayar2client', [BayarController::class, 'bayar2client'])->name('bayar2client');
-Route::get('detailsetujui', [SetujuController::class, 'detailsetujui'])->name('detailsetujui');
-Route::get('revisiselesai', [SelesaiController::class, 'revisiselesai'])->name('revisiselesai');
-Route::get('revisibutton', [SelesaiController::class, 'revisibutton'])->name('revisibutton');
-Route::get('detail-revisi-client', [SelesaiController::class, 'detail'])->name('detail-revisi-client');
+Route::middleware('auth')->group(function(){
 
+    Route::get('indexclient', [IndexcController::class, 'indexclient'])->name('indexclient');
+    Route::get('drequestclient', [IndexcController::class, 'drequestclient'])->name('drequestclient');
+    Route::get('createproreq', [IndexcController::class, 'createproreq'])->name('createproreq');
+    Route::post('simpanpro', [IndexcController::class, 'simpan'])->name('simpanpro');
+    Route::get('requestclient', [IndexcController::class, 'requestclient'])->name('requestclient');
+    Route::get('editproreq/{id}', [IndexcController::class, 'editproreq'])->name('editproreq');
+    Route::put('updateproreq/{id}', [IndexcController::class, 'update'])->name('updateproreq');
+    Route::get('setujuclient', [SetujuController::class, 'setujuclient'])->name('setujuclient');
+    Route::get('selesaiclient', [SelesaiController::class, 'selesaiclient'])->name('selesaiclient');
+    Route::get('revisiclient', [SelesaiController::class, 'revisiclient'])->name('revisiclient');
+    Route::get('ditolakclient', [TolakController::class, 'ditolakclient'])->name('ditolakclient');
+    Route::get('bayarclient', [BayarController::class, 'bayarclient'])->name('bayarclient');
+    Route::get('bayar2client', [BayarController::class, 'bayar2client'])->name('bayar2client');
+    Route::get('detailsetujui', [SetujuController::class, 'detailsetujui'])->name('detailsetujui');
+    Route::get('revisiselesai', [SelesaiController::class, 'revisiselesai'])->name('revisiselesai');
+    Route::get('revisibutton', [SelesaiController::class, 'revisibutton'])->name('revisibutton');
+    Route::get('detail-revisi-client', [SelesaiController::class, 'detail'])->name('detail-revisi-client');
+    });
+    
 // Halaman Admin
 Route::get('projectreq', [ProjectrequestController::class, 'projectreq'])->name('projectreq');
 Route::get('detailproreq/{id}', [ProjectrequestController::class, 'detailproreq'])->name('detailproreq');
