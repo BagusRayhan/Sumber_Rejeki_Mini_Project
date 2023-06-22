31116f4a45599d5c811c8dfa513113092a9954c5
ilhamvfx <65000447+muhammadilham879@users.noreply.github.com>
2023-06-22 10:16:33 +0700

    memperbaiki add fitur di client

--- resources/views/Client/createproreq.blade.php
+++ resources/views/Client/createproreq.blade.php
@@ -115,35 +115,16 @@
     <center>
         <button type="submit" class="btn btn-primary w-100">Kirim Request</button><br><br>
         <a href="http://127.0.0.1:8000/drequestclient">Kembali</a>
-    </center>
-</form>
-
-            </div>
+      </center>
+    </form>
         </div>
+            </div>
 <div class="card" style="width: 28em">
     <div class="card-body" style="height: 300px; overflow-y: scroll;">
         <h5 class="card-title">Fitur</h5>
         <form>
-            {{-- CODE START ICON TITIK 3 --}}
-            <div class="input-group">
-                <div class="dropdown-menu">
-                    <ul class="dropdown-options">
-                        <li data-bs-toggle="modal" data-bs-target="#detailfitur"><span class="option-icon"><i class="fa-solid fa-circle-plus"></i></span> Tambah deskripsi</li>
-                        <li data-bs-toggle="modal" data-bs-target="#"><span class="option-icon"><i class="fa fa-trash"></i></span> Hapus</li>
-                    </ul>
-                </div>
-            </div><br>
-            {{-- AKHIR CODE ICON TITIK 3 --}}
-
             {{-- CODE START ADD FITUR --}}
             <div id="container"></div>
-
-            <div class="input-group">
-                <input type="text" class="form-control" id="inpu" placeholder="Add fitur">
-                <div class="input-icon" style="z-index: 5">
-                    <i class="fa-solid fa-circle-plus" onclick="addInput(event)"></i>
-                </div>
-            </div><br>
 
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script>
@@ -158,58 +139,35 @@
                     var inputValue = document.getElementById('inpu').value;
                     inputElement.value = inputValue;
 
-                    var inputContainer = document.createElement('div');
-                    inputContainer.classList.add('input-container');
-
                     var inputGroup = document.createElement('div');
                     inputGroup.classList.add('input-group');
                     inputGroup.appendChild(inputElement);
 
-                    var ellipsisIcon = document.createElement('i');
-                    ellipsisIcon.classList.add('fas', 'fa-ellipsis-v');
-                    var ellipsisDiv = document.createElement('div');
-                    ellipsisDiv.classList.add('input-icon');
-                    ellipsisDiv.style.zIndex = 5;
-                    ellipsisDiv.appendChild(ellipsisIcon);
-                    inputGroup.appendChild(ellipsisDiv);
-
-                    var dropdownMenu = document.createElement('div');
-                    dropdownMenu.classList.add('dropdown-menu');
-                    var dropdownOptions = document.createElement('ul');
-                    dropdownOptions.classList.add('dropdown-options');
-                    var addDescriptionItem = document.createElement('li');
-                    addDescriptionItem.dataset.bsToggle = 'modal';
-                    addDescriptionItem.dataset.bsTarget = '#detailfitur';
-                    var addDescriptionSpan = document.createElement('span');
-                    addDescriptionSpan.classList.add('option-icon');
-                    var addDescriptionIcon = document.createElement('i');
-                    addDescriptionIcon.classList.add('fa-solid', 'fa-circle-plus');
-                    addDescriptionSpan.appendChild(addDescriptionIcon);
-                    addDescriptionItem.appendChild(addDescriptionSpan);
-                    addDescriptionItem.innerHTML += ' Tambah deskripsi';
-                    var deleteItem = document.createElement('li');
-                    deleteItem.dataset.bsToggle = 'modal';
-                    deleteItem.dataset.bsTarget = '#';
-                    var deleteSpan = document.createElement('span');
-                    var deleteButton = document.createElement('button');
-                    deleteButton.classList.add('deleteButton');
-                    deleteButton.textContent = 'Hapus';
-                    deleteButton.addEventListener('click', deleteInput);
-                    inputGroup.appendChild(deleteButton);
-                    deleteSpan.classList.add('option-icon');
-
-                    var deleteIcon = document.createElement('i');
-                    deleteIcon.classList.add('fa', 'fa-trash');
-                    deleteSpan.appendChild(deleteIcon);
-                    deleteItem.appendChild(deleteSpan);
-                    deleteItem.innerHTML += ' Hapus';
-                    dropdownOptions.appendChild(addDescriptionItem);
-                    dropdownOptions.appendChild(deleteItem);
-                    dropdownMenu.appendChild(dropdownOptions);
-                    inputGroup.appendChild(dropdownMenu);
-
+                    var inputContainer = document.createElement('div');
+                    inputContainer.classList.add('input-container');
                     inputContainer.appendChild(inputGroup);
                     container.appendChild(inputContainer);
+
+                    var inputIcon1 = document.createElement('div');
+                    inputIcon1.classList.add('input-icon');
+                    inputIcon1.style.marginRight = '28px'; // Menambahkan margin kanan
+
+                    var icon1 = document.createElement('i');
+                    icon1.classList.add('fa-solid', 'fa-file-circle-plus');
+                    icon1.addEventListener('click', showModal); // Menambahkan event listener pada ikon fa file circle plus
+                    inputIcon1.appendChild(icon1);
+
+                    var inputIcon2 = document.createElement('div');
+                    inputIcon2.classList.add('input-icon');
+                    inputIcon2.style.marginLeft = '5px'; // Menambahkan margin kiri
+
+                    var icon2 = document.createElement('i');
+                    icon2.classList.add('fa', 'fa-trash');
+                    icon2.addEventListener('click', deleteInput); // Menambahkan event listener pada ikon delete
+                    inputIcon2.appendChild(icon2);
+
+                    inputGroup.appendChild(inputIcon1);
+                    inputGroup.appendChild(inputIcon2);
 
                     // Reset nilai input
                     document.getElementById('inpu').value = '';
@@ -219,7 +177,25 @@
                         $(this).siblings('.dropdown-menu').toggle();
                     });
                 }
+
+                function deleteInput(event) {
+                    event.preventDefault();
+                    var inputContainer = event.target.parentNode.parentNode;
+                    inputContainer.remove();
+                }
+
+                function showModal(event) {
+                    event.preventDefault();
+                    $('#detailfitur').modal('show');
+                }
             </script>
+
+            <div class="input-group">
+                <input type="text" class="form-control" id="inpu" placeholder="Add fitur">
+                <div class="input-icon" style="z-index: 5">
+                    <i class="fa-solid fa-circle-plus" onclick="addInput(event)"></i>
+                </div>
+            </div><br>
 
             {{-- AKHIR CODE ADD FITUR --}}
             <script>
@@ -230,51 +206,38 @@
                     });
                 });
             </script>
-          <script>
-            function deleteInput(event) {
-    event.preventDefault();
-    var inputContainer = event.target.parentNode.parentNode;
-    inputContainer.remove();
-}
-
-          </script>
-            <center>
         </form>
     </div>
 </div>
 
-
-
-    </div>
-
-     <!-- Modal Box tambah desripsi Start -->
-     <div class="modal fade" id="detailfitur" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Tambah Deskripsi</h1>
-                    {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
-                </div>
-                <div class="modal-body">
-                    <form action="">
-
-                        <div class="col-sm-12 col-xl-11" style="margin-left: 2%; margin">
-                            <div class="mb-3">
-                                    <label for="input1">Deskripsi</label>
-                                    <textarea class="form-control w-100" name="deskirpsi" placeholder="Masukkan deskripsi yang anda inginkan"></textarea>
-                            </div>
-                            </div>
-                        <div class="col-sm-12 col-xl-11 d-flex justify-content-end" style="margin-left: 2%; margin">
-                            <div class="mb-3">
-                                    <a href="" class="btn btn-danger">Batal</a>
-                                    <a href="" class="btn btn-primary">Simpan</a>
-                            </div>
-                       </form>
-                  </div>
-
+<!-- Modal Box tambah desripsi Start -->
+<div class="modal fade" id="detailfitur" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Tambah Deskripsi</h1>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <div class="col-sm-12 col-xl-11" style="margin-left: 2%; margin">
+                        <div class="mb-3">
+                            <label for="input1">Deskripsi</label>
+                            <textarea class="form-control w-100" name="deskripsi" placeholder="Masukkan deskripsi yang anda inginkan"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-xl-11 d-flex justify-content-end" style="margin-left: 2%; margin">
+                        <div class="mb-3">
+                            <a href="" class="btn btn-danger" data-bs-dismiss="modal">Batal</a>
+                            <a href="" class="btn btn-primary">Simpan</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+    </div>
+
 <!-- Modal Box Edit Bank End-->
 
 
