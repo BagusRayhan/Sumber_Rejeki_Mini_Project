364abea5149f21817fabeda124014f2234032317
ddckyy01 <130640162+ddckyy01@users.noreply.github.com>
2023-07-13 10:37:27 +0700

    estimasi,pembayaran pending,modal proreq

--- resources/views/Client/index.blade.php
+++ resources/views/Client/index.blade.php
@@ -91,7 +91,7 @@
                                                     <div class="bg-light rounded h-100 p-10">
                                                         <div class="pg-bar mb-3">
                                                             <div class="progress">
-                                                                <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                                                <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">{{ $estimasisetuju->progress }}</div>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -101,38 +101,45 @@
                                     </div>
                                 @endif
                             @endforeach
-                        @else
-                            <p>Tidak ada estimasi yang tersedia.</p>
+                            @else
+                            <div class="d-flex flex-column h-100 justify-content-center align-items-center">
+                                <img src="gambar/empty-icon/empty-directory.png" style="width:65px;" >
+                                <p>Tidak ada pesan masuk</p>
+                            </div>
                         @endif
                         </div>
                         </div>
 
+
                         <div class="col-sm-12 col-md-6 col-xl-4">
-                        <div class="h-100 bg-light rounded p-4">
-                        <div class="d-flex align-items-center justify-content-between mb-2">
-                        <h6 class="mb-0">Pesan</h6>
-                        {{-- <a class="link-offset-2 link-underline link-underline-opacity-0" href="#">Tampilkan Semua</a> --}}
+                            <div class="h-100 bg-light rounded p-4">
+                                <div class="d-flex align-items-center justify-content-start mb-2">
+                                    <h6 class="mb-0">Pesan</h6>
+                                </div>
+                                @if (count($pesancht) !== 0)
+                                    @foreach ($pesancht as $pesan)
+                                        <a href="{{ route('detailsetujui', ['id' => $pesan->id]) }}" style="text-decoration: none; color: inherit;">
+                                            <div class="d-flex align-items-center border-bottom py-3">
+                                                <img class="rounded-circle flex-shrink-0" src="/gambar/user-profile/{{ $pesan->user->profil }}" alt="" style="width: 40px; height: 40px;">
+                                                <div class="w-100 ms-3">
+                                                    <div class="d-flex w-100 justify-content-between">
+                                                        <h6 class="mb-0">{{ $pesan->user->name }}</h6>
+                                                        <small>{{ Carbon::parse($pesan->chat_time)->locale('id')->isoFormat('HH:MM') }}</small>
+                                                    </div>
+                                                    <span>{{ $pesan->chat }}</span>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    @endforeach
+                                @else
+                                    <div class="d-flex flex-column h-100 justify-content-center align-items-center">
+                                        <img src="gambar/empty-icon/empty-directory.png" class="w-25">
+                                        <p>Tidak ada pesan masuk</p>
+                                    </div>
+                                @endif
+                            </div>
                         </div>
-                        @if (count($pesancht) !== 0)
-                        @foreach ($pesancht as $pesan)
-                            <div class="d-flex align-items-center border-bottom py-3">
-                                <a href="{{ route('detailsetujui', ['id' => $pesan->id]) }}" class="d-flex w-100" style="text-decoration: none; color: inherit;">
-                                    <img class="rounded-circle flex-shrink-0" src="/gambar/user-profile/{{ $pesan->user->profil }}" alt="" style="width: 40px; height: 40px;">
-                                    <div class="w-100 ms-3">
-                                    <div class="d-flex w-100 justify-content-between">
-                                        <h6 class="mb-0">{{ $pesan->user->name }}</h6>
-                                        <small>{{ Carbon::parse($pesan->chat_time)->locale('id')->isoFormat('HH:MM') }}</small>
-                                        </div>
-                                        <span>{{ $pesan->chat }}</span>
-                        </div>
-                        </a>
-                </div>
-            @endforeach
-        @else
-            <p>Tidak ada pesan yang tersedia.</p>
-        @endif
-    </div>
-</div>
+
 
                     </div>
                 </div>
