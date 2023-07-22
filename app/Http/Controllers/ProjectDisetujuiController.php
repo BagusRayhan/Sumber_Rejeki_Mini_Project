0b59de9112e69ef121f0293e88bfe2393867e518
Diky <130640162+ddckyy01@users.noreply.github.com>
2023-07-22 11:46:33 +0700

    push

--- app/Http/Controllers/ProjectDisetujuiController.php
+++ app/Http/Controllers/ProjectDisetujuiController.php
@@ -18,7 +18,7 @@ class ProjectDisetujuiController extends Controller
     public function disetujui(Request $request ) {
 
              $today = date('Y-m-d');
-            Proreq::whereDate('deadline', '<=', $today)->update(['status2' => 'telat']);
+            Proreq::whereDate('deadline', '<', $today)->update(['status2' => 'telat']);
 
         if ($request->ajax()) {
             $data = Proreq::latest()->get();
