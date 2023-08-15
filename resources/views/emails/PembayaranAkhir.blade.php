2662bacc0f0eb759438d02ec8fb9fc8cf72961bf
ilhamvfx <133854062+muhammadilham1001@users.noreply.github.com>
2023-08-15 20:30:54 +0700

    memperbaiki tampilan notif email

--- resources/views/emails/PembayaranAkhir.blade.php
+++ resources/views/emails/PembayaranAkhir.blade.php
@@ -5,37 +5,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pembayaran Akhir</title>
-    <style>
+      <style>
         body {
-        font-family: Arial;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f5f5f5;
         }
 
         .coupon {
-        border: 5px #bbb;
-        width: 80%;
-        border-radius: 15px;
-        margin: 0 auto;
-        max-width: 600px;
+            border: 5px solid #bbb;
+            width: 80%;
+            border-radius: 15px;
+            margin: 0 auto;
+            max-width: 600px;
+            background-color: white;
         }
+
         .header {
             display: flex;
-            padding: 2px 16px;
+            align-items: center;
+            padding: 10px 20px;
             background-color: #f1f1f1;
         }
-        .container {
-        padding: 2px 16px;
-        background-color: #f1f1f1;
+
+        .header img {
+            width: 50px;
+            height: 50px;
+            margin-right: 10px;
         }
-        .container span {
-            margin-top: 20px;
-            margin-bottom: 15px;
+
+        .header h2 {
+            margin: 0;
+            font-size: 1.5em;
+        }
+
+        .container {
+            padding: 20px;
+        }
+
+        h3 {
+            font-size: 1.5em;
+            margin-top: 0;
+        }
+
+        p {
+            font-size: 1.1em;
+            line-height: 1.6;
+        }
+
+        .footer {
+            padding: 15px;
+            text-align: center;
+            background-color: #f1f1f1;
+            font-size: 0.9em;
         }
     </style>
 </head>
 <body>
     <div class="coupon">
         <div class="header">
-            <img src="https://i.postimg.cc/xTt6vNQN/logo.png" style="width: 3em; height: 2em; margin: 1em 0">
+            <img src="https://i.postimg.cc/xTt6vNQN/logo.png" alt="PROREQ Logo">
             <div class="wrapper" style="display: grid; align-items:center; padding:0;">
                 <h2><b>PROREQ Company</b></h2>
             </div>
