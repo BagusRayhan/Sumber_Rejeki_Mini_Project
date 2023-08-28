7da846b6055aa7e4b7a96a74126d127b7e73711c
ddckyy01 <130640162+ddckyy01@users.noreply.github.com>
2023-08-28 10:16:16 +0700

    mengganti pesan error
    mengganti pesan error saat halaman yang dicari tidak ada

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,28 +2,38 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tidak Tersedia</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Halaman Tidak Tersedia</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+
+	<script>
+		document.addEventListener("DOMContentLoaded", function() {
+			var goHomeButton = document.querySelector("#notfound a");
+			goHomeButton.addEventListener("click", function(event) {
+				event.preventDefault();
+				history.back();
+			});
+		});
+	</script>
+
 </head>
+
 <body>
 
-    <div id="notfound">
-        <div class="notfound">
-            <div class="notfound-404">
-                <h1>4<span>0</span>4</h1>
-            </div>
-            <h2>Halaman yang Anda minta tidak dapat ditemukan</h2>
-            <a href="{{ url()->previous() }}" class="back-button">Kembali</a>
-            {{-- <form class="notfound-search">
-                <input type="text" placeholder="Search...">
-                <button type="button"><span></span></button>
-            </form> --}}
-        </div>
-    </div>
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>Oops!</h1>
+			</div>
+			<h2>404 - Page not found</h2>
+			<p>Halaman yang Anda cari mungkin telah dihapus, namanya diubah, atau untuk sementara tidak tersedia.</p>
+			<a href="#">Return to previous page</a>
+		</div>
+	</div>
 
 </body>
 
