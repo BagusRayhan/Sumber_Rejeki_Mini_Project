c93051a5de233afad8506eb11296427f4b0d2be0
ilhamvfx <133854062+muhammadilham1001@users.noreply.github.com>
2023-09-08 08:44:02 +0700

    style footer

--- resources/views/Client/Template/footer.blade.php
+++ resources/views/Client/Template/footer.blade.php
@@ -1,5 +1,6 @@
-<div style="margin-top: 24%;" style="position: fixed;">
-  <footer class="text-center text-white" style="background-color: #F3F6F9;">
+<div style="position: fixed;   bottom: 0;
+  width: 100%;">
+  <footer class="text-center text-white" style="background-color: #F3F6F9;  margin-left: -20em;">
     <div class="container pt-800">
       <!-- Section: Social media -->
       <section class="mb-500">
