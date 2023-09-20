f64d0e607ea036cc82210164a1691e83a75a9d88
Harja <rachmadsuharja123@gmail.com>
2023-09-20 20:18:25 +0700

    method not allowed
    menambahkan tampilan method not allowed

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -6,13 +6,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Halaman Tidak Tersedia</title>
+	<title>Method Not Allowed</title>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}" />
 
 	<script>
 		document.addEventListener("DOMContentLoaded", function() {
-			var goHomeButton = document.querySelector("#notfound a");
+			var goHomeButton = document.querySelector("#notallowed a");
 			goHomeButton.addEventListener("click", function(event) {
 				event.preventDefault();
 				history.back();
@@ -24,13 +24,13 @@
 
 <body>
 
-	<div id="notfound">
-		<div class="notfound">
-			<div class="notfound-404">
+	<div id="notallowed">
+		<div class="notallowed">
+			<div class="notallowed-405">
 				<h1>Oops!</h1>
 			</div>
-			<h2>404 - Page not found</h2>
-			<p>Halaman yang Anda cari mungkin telah dihapus, namanya diubah, atau untuk sementara tidak tersedia.</p>
+			<h2>405 - Method not allowed</h2>
+			<p>Metode yang Anda gunakan tidak diizinkan.</p>
 			<a href="#">Return to previous page</a>
 		</div>
 	</div>
