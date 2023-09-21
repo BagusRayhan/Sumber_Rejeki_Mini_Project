6c50612b20a09a2c8ab519cb85389f7415aad311
Harja <rachmadsuharja123@gmail.com>
2023-09-21 14:38:09 +0700

    Create 419.blade.php

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Page Expired</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}" />
+
+	<script>
+		document.addEventListener("DOMContentLoaded", function() {
+			var goHomeButton = document.querySelector("#notfound a");
+			goHomeButton.addEventListener("click", function(event) {
+				event.preventDefault();
+				history.back();
+			});
+		});
+	</script>
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>Oops!</h1>
+			</div>
+			<h2>419 - Page expired</h2>
+			<p>Sesi autentikasi gagal atau telah kadaluwarsa</p>
+			<a href="#">Return to previous page</a>
+		</div>
+	</div>
+
+</body>
+
+</html>
